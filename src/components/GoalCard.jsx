0ecaaec4480f184461bfb08370aaaec297ce7059
bokeum <bokmail83@gmail.com>
2025-02-07 18:06:02 +0900

    modify UI of GoalCard

--- src/components/GoalCard.jsx
+++ src/components/GoalCard.jsx
@@ -1,19 +1,24 @@
-import { ClipboardIcon } from '@heroicons/react/24/solid';
+import { FireIcon, PencilIcon } from '@heroicons/react/24/solid';
 import { useNavigate } from 'react-router-dom';
 import '../styles/GoalCard.css';
 
 const GoalCard = ({ goal }) => {
   const navigate = useNavigate();
+  
   return (
     <div className="goal-card">
-      <h2 className="goal-card-title">
-        <ClipboardIcon className="goal-card-icon" />
-        오늘의 목표
-      </h2>
-      <p className="goal-card-text">{goal || '설정된 목표가 없습니다.'}</p>
-      <button className="goal-card-button" onClick={() => navigate('/goal')}>
-        목표 설정
-      </button>
+      <div className="goal-card-header">
+        <h2 className="goal-card-title">
+          <FireIcon className="goal-card-icon" />
+          오늘의 목표
+        </h2>
+        <button className="goal-edit-button" onClick={() => navigate('/goal')}>
+          <PencilIcon className="edit-icon" />
+        </button>
+      </div>
+      <p className="goal-card-text">
+        {goal || "아직 목표가 설정되지 않았습니다. 지금 설정해보세요!"}
+      </p>
     </div>
   );
 };
